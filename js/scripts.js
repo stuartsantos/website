308a4c0fe147200261a048257c5bc4da403d5419
Stuart Santos <santos.stuart@gmail.com>
2020-08-29 21:51:36 -0400

    updated jquery
    updated jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,28 @@
 //smooth scroll
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
       var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
       if (target.length) {
-        $('html,body').animate({
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
           scrollTop: target.offset().top
-        }, 1000);
-        return false;
+        }, 1000, function() {
+          // Callback after animation          
+        });
       }
     }
   });
-});
